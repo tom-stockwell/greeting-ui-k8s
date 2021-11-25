70ca8facfcf03add2fae6590c0b2d564ece4f54b
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2021-11-25 12:22:21 +0000

    Initial implementation

---
+++ environments/production/env.js
@@ -0,0 +1,3 @@
+window.env = {
+	REACT_APP_API_URL: 'https://api.example-app.apps.paas.lab.stocky37.dev',
+}
---
+++ environments/staging/env.js
@@ -0,0 +1,3 @@
+window.env = {
+	REACT_APP_API_URL: 'https://staging.api.example-app.apps.paas.lab.stocky37.dev',
+}
