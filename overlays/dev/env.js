b24c63aaef681c9c3d428b8fe0df6baf9c36cf17
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2022-06-13 13:12:11 +0100

    update for new demo-gitops-cicd tenant

--- overlays/dev/env.js
+++ overlays/dev/env.js
@@ -1,3 +1,3 @@
 window.env = {
-	REACT_APP_API_URL: 'https://dev.api.greeting.examples.apps.paas.lab.stocky37.dev',
+	REACT_APP_API_URL: 'https://greeting-api.demo-gitops-cicd-dev.apps.paas.lab.stocky37.dev',
 }
--- overlays/stage/env.js
+++ overlays/stage/env.js
@@ -1,3 +1,3 @@
 window.env = {
-	REACT_APP_API_URL: 'https://stage.api.greeting.examples.apps.paas.lab.stocky37.dev',
+	REACT_APP_API_URL: 'https://greeting-api.demo-gitops-cicd-stage.apps.paas.lab.stocky37.dev',
 }
